df484445e70bdd7d5f1263fcb41fbc80570358f3
Richmond Eribo <uyiosaeribo344@gmail.com>
2024-11-03 17:03:55 +0100

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react()],
-  adapter: vercel(),
+  adapter: netlify(),
 })
