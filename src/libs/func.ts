6f8b8cac1921ff6b6fbbc642480d2e609bcb2266
Richmond Eribo <uyiosaeribo344@gmail.com>
2024-11-13 17:20:47 +0100

    config embla with intersection observer

--- src/libs/func.ts
+++ src/libs/func.ts
@@ -21,3 +21,40 @@ export const escapeHTML = (str: string): string => {
     char => htmlEscapes[char as keyof typeof htmlEscapes]
   )
 }
+
+interface ObserverOptions extends IntersectionObserverInit {}
+type ObserverCallback = (entry: IntersectionObserverEntryInit) => void
+
+export class IntersectionObserverHelper {
+  private observer: IntersectionObserver
+  private callback: ObserverCallback
+  private options: ObserverOptions
+
+  constructor(callback: ObserverCallback, options: ObserverOptions = {}) {
+    this.options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+      ...options,
+    }
+    this.callback = callback
+
+    //   create the observer
+
+    this.observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        this.callback(entry)
+      })
+    }, this.options)
+  }
+
+  observerElements(selector: string): void {
+    const elements = document.querySelectorAll(selector)
+    elements.forEach(element => this.observer.observe(element))
+  }
+
+  unobserveElement(selector: string): void {
+    const elements = document.querySelectorAll(selector)
+    elements.forEach(element => this.observer.unobserve(element))
+  }
+}
